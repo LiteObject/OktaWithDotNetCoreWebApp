c963429cbedd099415f4dd5619998167ee922191
Mohammed <tuniis@live.com>
2019-10-20 22:09:42 -0500

    Completed initial okta setup

---
+++ OktaWithDotNetCoreWebApp/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿namespace OktaWithDotNetCoreWebApp.Controllers
+{
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.AspNetCore.Mvc;
+
+    using Okta.AspNetCore;
+
+    /// <summary>
+    /// The account controller.
+    /// </summary>
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// The login.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
+        public IActionResult Login()
+        {
+            if (!this.HttpContext.User.Identity.IsAuthenticated)
+            {
+                return this.Challenge(OktaDefaults.MvcAuthenticationScheme);
+            }
+
+            return this.RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>
+        /// The logout.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            return new SignOutResult(
+                new[]
+                    {
+                        OktaDefaults.MvcAuthenticationScheme,
+                                             CookieAuthenticationDefaults.AuthenticationScheme
+                                         },
+            new AuthenticationProperties { RedirectUri = "/Home/" });
+        }
+    }
+}
--- OktaWithDotNetCoreWebApp/Controllers/HomeController.cs
+++ OktaWithDotNetCoreWebApp/Controllers/HomeController.cs
@@ -1,29 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using OktaWithDotNetCoreWebApp.Models;
-
-namespace OktaWithDotNetCoreWebApp.Controllers
+﻿namespace OktaWithDotNetCoreWebApp.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.CodeAnalysis;
+
+    using OktaWithDotNetCoreWebApp.Models;
+
+    /// <summary>
+    /// The home controller.
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// The index.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
         public IActionResult Index()
         {
-            return View();
+            return this.View();
         }
 
+        /// <summary>
+        /// The privacy.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
         public IActionResult Privacy()
         {
-            return View();
+            return this.View();
         }
 
+        /// <summary>
+        /// The admin.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
+        [Authorize]
+        public IActionResult Admin()
+        {
+            return this.View(this.HttpContext.User.Claims);
+        }
+
+        /// <summary>
+        /// The error.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IActionResult"/>.
+        /// </returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
         }
     }
 }
